622012a457fb5d48e7aaa5855cb6b153728b74a3
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2018-11-10 14:29:01 +0530

    create prediction model

--- app/analytics/prediction_model.py
+++ app/analytics/prediction_model.py
@@ -1,15 +1,30 @@
-# -*- coding: utf-8 -*-
+import app.data.data_parser as dp
+from sklearn.externals import joblib
+
+subjects = ["Art","Citizenship_Education","English","Geography","Health","History","Mathematics","PTS","Religion","Science","Sinhala","Tamil"]
+
+def get_prediction_marks(df,subject):
+    xgb_regressor = joblib.load('joblibs/xgb/'+subject+'_xgb.joblib')
+    return xgb_regressor.predict(df)
+
 def get_prediction(df):
-    return {
-        "Sinhala": 50,
-        "Maths": 50,
-        "Science": 50,
-        "Art": 50,
-        "Dance": 50,
-        "Buddhism" :50,
-        "History": 50,
-        "Tamil" : 50,
-        "English": 50,
-        "Geology" : 50,
-        "LearningStyle" : 2
-    }
+    
+    for subject in subjects:
+
+        df = dp.generate_dataset (df,[subject])
+        
+        return {
+            "Sinhala": get_prediction_marks(df,subject),
+            "Maths": get_prediction_marks(df,subject),
+            "Science": get_prediction_marks(df,subject),
+            "Art": get_prediction_marks(df,subject),
+            "Dance": get_prediction_marks(df,subject),
+            "Buddhism" :get_prediction_marks(df,subject),
+            "History": get_prediction_marks(df,subject),
+            "Tamil" : get_prediction_marks(df,subject),
+            "English": get_prediction_marks(df,subject),
+            "Geology" : get_prediction_marks(df,subject),
+            "LearningStyle" : 2
+        }
+    
+
